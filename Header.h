f0d98cd638d25e365f00a92387408d7b0ad79d82
Mercy <127452761+S1lent-N1ght@users.noreply.github.com>
2025-11-11 23:42:53 +0000

    Add includes and comment on OOP importance
    Added includes for iostream and string, and a comment on OOP.

--- Header.h
+++ Header.h
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <string>
 
+//implementation a class 
+
+
+
+
+/* A reason why OOP is important is because 
+it allows for more code to be added without breaking the entire program.*/
